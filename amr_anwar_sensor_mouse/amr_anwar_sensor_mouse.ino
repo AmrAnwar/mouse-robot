647339406fa467ba08df036b7a0f80095e077d95
amr anwar <amranwar@amrs-MacBook-Air.local>
2017-11-26 16:18:54 +0200

    finish left most

--- amr_anwar_sensor_mouse/amr_anwar_sensor_mouse.ino
+++ amr_anwar_sensor_mouse/amr_anwar_sensor_mouse.ino
@@ -1,7 +1,10 @@
-int m1[2] = {6, 7};
-int m2[] = {8,9};
+// front, back, pwm
+int m1[] = {6, 7, 8};
+int m2[] = {9,10 , 11};
 int sensors[] = {A0, A1, A2, A3};
-
+int vel_m1 = 100 ;
+int vel_m2 = 100 ;
+bool flag = false;
 void setup() {
   // put your setup code here, to run once:
   for ( int i = 0 ; i < 2 ; i++){
@@ -22,48 +25,45 @@ void loop() {
   motor_move(values);
   
   for (int i =0 ; i < 3 ;i ++ ){
-    values[i] = digitalRead(sensors[i]);
+    values[i] = analogRead(sensors[i]);
     motor_move(values);     
   }
   
 }
 
-void rotation(int right){
-  if (right){
-      digitalWrite(m1[0],1);
-      digitalWrite(m2[1],0);
-  }
-  else {
-      digitalWrite(m1[1],1);
-      digitalWrite(m2[0],0);
-    }
-  delay(2000);
-}
 void motor_move(int values[]){
   /* i = 0 up 
      i = 1 left 
      i = 2 right 
   */
-   if(values[0]){
+  
+   if(values[1] > 1000 ){
       digitalWrite(m1[0],1);
       digitalWrite(m2[0],1);
+      analogWrite(m1[2], vel_m1); // speed
+      analogWrite(m2[2], vel_m2);
     }
-   else if((values[1])){
-          motor_speed(0);
+   else if((values[0] > 1000)){
+          motor_speed();
           digitalWrite(m1[0],1);
+          analogWrite(m1[2], vel_m1);
           digitalWrite(m2[1],0);
           delay(2000);
     }
-   else if ((values[2])){
-          motor_speed(0);
-          digitalWrite(m1[1],1);
+   else if (values[2] > 1000){
+      motor_speed();
+      digitalWrite(m1[1],1);
+      analogWrite(m1[2], vel_m1);
       digitalWrite(m2[0],0);
       delay(2000);
     }
     
    else {
-      digitalWrite(m1[0],0);
-      digitalWrite(m2[0],0);
+    motor_speed();
+    digitalWrite(m1[1],1);
+    digitalWrite(m2[1],1);
+    analogWrite(m1[2], vel_m1); // speed
+    analogWrite(m2[2], vel_m2);
    }
 //   else if(left){
 //    
@@ -71,10 +71,10 @@ void motor_move(int values[]){
      
   }
 
-void motor_speed(int speed){
-  for ( int i = 0 ; i < 2 ; i++){
-      digitalWrite(m1[i], speed);
-      digitalWrite(m2[i], speed);
+void motor_speed(){
+  for ( int i = 0 ; i < 3 ; i++){
+      analogWrite(m1[i], 0);
+      analogWrite(m2[i], 0);
     }
   }
   
